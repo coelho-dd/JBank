d6e2089541dff05fef50b203db87e786513d3437
Davi Coelho <davicoelho.cont@gmail.com>
2024-10-16 23:22:12 -0300

    Menu prototype done

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        User user;
         String userInput;
 
         System.out.println("WELCOME TO JBANK!!!");
@@ -16,13 +17,16 @@ public class Main {
         userInput = userInput.toLowerCase();
         
         if(userInput.equals("yes")) {
-            registration();
+            user = registration();
         } else {
             System.out.println("Come back any time!");
+            return;
         }
+
+        menu(user, scanner);
     }
 
-    public static void registration() {
+    public static User registration() {
         Scanner scanner = new Scanner(System.in);
 
         String name = validateInput("Name", scanner);
@@ -30,9 +34,9 @@ public class Main {
         String password = validateInput("Password", scanner);
 
         User user = new User(name, email, password);
-        System.out.println("Registration successfull for " + user.getName());
+        System.out.println("Registration successfull for user: " + user.getName() + ".\n");
 
-        scanner.close();
+        return user;
     }
 
     public static String validateInput(String data, Scanner scanner) {
@@ -45,5 +49,62 @@ public class Main {
             }
         } while(input.isBlank());
         return input;
+    }
+
+    public static void menu(User user, Scanner scanner) {
+        String input;
+
+        System.out.println("ACCOUNT MENU");
+        System.out.println("=====================================\n");
+
+        System.out.println("To select an option below, enter their respective numbers:");
+        System.out.println("1- Create savings account");
+        System.out.println("2- Create checking account");
+        System.out.println("3- Deposit");
+        System.out.println("4- Withdraw");
+        System.out.println("5- Transfer");
+        System.out.println("6- Check banking statements");
+        System.out.println("7- Check balance");
+        System.out.println("8- Simulate a loan");
+        System.out.println("9- Simulate interest operation");
+        System.out.println("0- Log off");
+
+        while(true) {
+            input = scanner.nextLine();
+            switch(input) {
+                case "1":
+                    System.out.println("created a savings account.");
+                    break;
+                case "2":
+                    System.out.println("created checking account");
+                    break;
+                case "3":
+                    System.out.println("deposited");
+                    break;
+                case "4":
+                    System.out.println("just did that");
+                    break;
+                case "5":
+                    System.out.println("yes");
+                    break;
+                case "6":
+                    System.out.println("did it again");
+                    break;
+                case "7":
+                    System.out.println("another one");
+                    break;
+                case "8":
+                    System.out.println("just like the last one");
+                    break;
+                case "9":
+                    System.out.println("the last one");
+                    break;
+                case "0":
+                    System.out.println("Logging you off...");
+                    System.exit(0);
+                default:
+                    System.out.println("Option not valid. Try again");
+            }
+        }
     }
 }
